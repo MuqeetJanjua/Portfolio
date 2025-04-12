cf008559a9d18011e9bd233e51a29c28d2538962
muqeet <muqeetahmad25@gmail.com>
2025-04-12 18:08:01 +0500

    - vercel analytics added

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
+      <Analytics mode="production" />
       <Navbar />
       <main>
         <Hero />
